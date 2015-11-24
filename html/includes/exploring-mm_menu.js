cecd4efdb28c8bbdc7e23541afce0c61f8a51da6
MaxHodges <max@whiterabbitjapan.com>
2015-11-24 19:39:05 +0900

    git init

---
+++ html/includes/exploring-mm_menu.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 3:52:19 Apr 3, 2013 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 14:03:32 May 3, 2014.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>404 Not Found</title>
+</head><body>
+<h1>Not Found</h1>
+<p>The requested URL /exploring/mm_menu.js was not found on this server.</p>
+</body></html>
